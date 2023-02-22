b13d0c6238edfd445ba19d8eb5e9b2d681056d61
michel <michel-gouveia1997@hotmail.com>
2023-02-22 09:33:29 -0800

    Guess my number

--- js/main.js
+++ js/main.js
@@ -1,53 +1,51 @@
-function messagem(msg){
+'use strict'
+//let randomNumber = Math.trunc(Math.random() * 20) + 1
+let score  = 20
+const messagem = function(msg){
     document.querySelector('.messagem').textContent = msg
 }
-function inputNumber(guessNumber){
+const inputNumber = function(guessNumber){
     document.querySelector('.number').textContent = guessNumber
 }
-function scoreFunction(points){
+const  scoreFunction = function(points){
     document.querySelector('.score').textContent = points
 }
 
-function color(colorBody){
+const color = function(colorBody){
     document.querySelector('body').style.background = colorBody
 }
-let score  = 20
-'use strict'
-document.querySelector('.btn-check').addEventListener('click',function(){
 
+document.querySelector('.btn-check').addEventListener('click',function(){
     const input = Number(document.querySelector('#input').value)
     const randomNumber = Math.trunc(Math.random() * 20) + 1
     
     if(!input){
         messagem('⛔ No number')
-    }else if(randomNumber == input){
+    }else if(input === randomNumber){
         inputNumber(randomNumber)
         messagem('🎉 Correct number!')
         color('#60b347')
     }else if(input > randomNumber){
-        if(score > 0){
-            messagem('📉 Too high')
+        if(score > 1){
+            messagem('📈 Too high')
             scoreFunction(score)
             score--
-        }else if(score < 0){
-            messagem('💥 You lost the game')
+        }else{
+            messagem('💥 You lost the game!')
         }
         
     }else if(input < randomNumber){
-        messagem('📈 Too low')
-        score--
+        messagem('📈 Too high')
         scoreFunction(score)
+        score--
     }
-    
 })
 //Reset
 document.querySelector('.btn-again').addEventListener('click',function(){
     messagem('Start guessing...')
 
-    document.querySelector('.score').textContent = 20
-
-    scoreFunction(20)
-
+    scoreFunction(score)
+ 
     inputNumber('?')
 
     color('#000')
