83ca9d01eca216d1ec8062fa4f5ca0eb0d260a4c
michel <michel-gouveia1997@hotmail.com>
2023-02-22 06:31:32 -0800

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
+function messagem(msg){
+    document.querySelector('.messagem').textContent = msg
+}
+function inputNumber(guessNumber){
+    document.querySelector('.number').textContent = guessNumber
+}
+function scoreFunction(points){
+    document.querySelector('.score').textContent = points
+}
+
+function color(colorBody){
+    document.querySelector('body').style.background = colorBody
+}
+let score  = 20
 'use strict'
-
-console.log(document.querySelector('.messagem').textContent = '🎉 Correct number!')
-
-
-document.querySelector('.score').textContent = 25
-
-
 document.querySelector('.btn-check').addEventListener('click',function(){
-    const input = document.querySelector('#input').value
 
+    const input = Number(document.querySelector('#input').value)
+    const randomNumber = Math.trunc(Math.random() * 20) + 1
+    
+    if(!input){
+        messagem('⛔ No number')
+    }else if(randomNumber == input){
+        inputNumber(randomNumber)
+        messagem('🎉 Correct number!')
+        color('#60b347')
+    }else if(input > randomNumber){
+        if(score > 0){
+            messagem('📉 Too high')
+            scoreFunction(score)
+            score--
+        }else if(score < 0){
+            messagem('💥 You lost the game')
+        }
+        
+    }else if(input < randomNumber){
+        messagem('📈 Too low')
+        score--
+        scoreFunction(score)
+    }
+    
+})
+//Reset
+document.querySelector('.btn-again').addEventListener('click',function(){
+    messagem('Start guessing...')
 
-    console.log(input)
+    document.querySelector('.score').textContent = 20
 
+    scoreFunction(20)
 
-    document.querySelector('.number').textContent = input
+    inputNumber('?')
+
+    color('#000')
 })
 
 
